6ead040c53a65fdc317bfdab270b295e91b83101
sagar khadka <sagar.khadka2001@gmail.com>
2023-04-12 09:41:05 +0545

    Initial commit

---
+++ Untitled-1.py
@@ -0,0 +1,18 @@
+# %%
+print("App")
+
+# %%
+print(1)
+
+# %% [markdown]
+#
+
+# %%
+print(1+2)
+
+
+# %%
+for x in range(1, 1):
+    print("x")
+
+# %%
---
+++ app.py
@@ -0,0 +1,10 @@
+counter = 0
+
+while counter < 5:
+    counter = counter+1
+    if (counter == 3):
+        continue
+
+    print(counter)
+else:
+    print("Here")
